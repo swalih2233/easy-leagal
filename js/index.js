546b682a41c3c00d2dd0dd650d206da4a3edd386
swalih <swalihkpx@gmail.com>
2023-10-27 17:25:47 +0530

    first release

---
+++ js/index.js
@@ -0,0 +1,50 @@
+@font-face {
+  font-family: 'martelbold';
+  src: url('../fonts/martel-bold-webfont.woff2') format('woff2'),
+       url('../fonts/martel-bold-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+
+
+@font-face {
+  font-family: 'martelregular';
+  src: url('../fonts/martel-regular-webfont.woff2') format('woff2'),
+       url('../fonts/martel-regular-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+@font-face {
+  font-family: 'Montserrat-Bold';
+  src: url('../fonts/Montserrat-Bold.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+
+
+@font-face {
+  font-family: 'Montserrat-Regular';
+  src:url('../fonts/Montserrat-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+@font-face {
+  font-family:'OpenSans_Condensed-Regular';
+  src: url('../fonts/OpenSans_Condensed-Regular_1.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+
+
+@font-face {
+  font-family: 'OpenSans_Condensed-Bold';
+  src:url('../fonts/OpenSans_Condensed-Bold_1.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
